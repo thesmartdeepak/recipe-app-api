f64176be54b1c92fe9fcd4cad2d422bc34651235
Deepak <thesmartdeepak@gmail.com>
2023-11-28 09:49:47 +0530

    Update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ ENV PYTHONBUFFERED 1
 
 COPY requirements.txt /tmp/requirements.txt
 COPY requirements.dev.txt /tmp/requirements.dev.txt
-COPY app ./app
+COPY . ./app
 
 RUN ls
 
--- app/app/settings.py
+++ app/app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '''django-insecure-qjp98nj!f%dr^vt$zx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
